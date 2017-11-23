49e88582a6c543055a5f339675d7e9cff25ddc12
lnc <lnc@DIN76000819.corp.capgemini.com>
2017-11-23 16:43:33 +0530

    first

---
+++ myDemo/src/main/java/org/capge/MainClass.java
@@ -0,0 +1,16 @@
+package org.capge;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		greetings();
+
+	}
+	
+	public static void greetings()
+	{
+		System.out.println("hello world");
+	}
+
+}
